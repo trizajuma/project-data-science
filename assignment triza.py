8aaa73132bc7841672fa80810ce8e0703f2cf9a6
trizajuma <jumatriza8@gmail.com>
2024-06-30 22:28:19 +0300

    Add files via upload

---
+++ assignment triza.py
@@ -0,0 +1,15 @@
+name= "Triza Juma"
+math=67
+english=76
+science=72
+average=(math+science+english)/3
+print(average)
+subjects=[math,english,science]
+print(subjects)
+Trizadict={
+"name": "Triza Juma",
+"math":67,
+"science":72,
+"english":76,
+}
+print(Trizadict)
